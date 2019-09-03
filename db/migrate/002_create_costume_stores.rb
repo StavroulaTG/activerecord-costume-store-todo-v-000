ee4b2c026f99df04243dae201c5976a388dada04
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-03 15:12:54 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :create_costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.boolean :still_in_business
+      t.timestamps
+    end
+  end
+end
