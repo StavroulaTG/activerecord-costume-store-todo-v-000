42893fbb42658d45f55873af51cab408d7db9cbe
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-03 14:36:36 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,10 +4,12 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :costume_inventory
-      t.integer :num_of_employees
-      t.boolean :still_in_business
-      t.timestamps
+      t.string :costume_inventory
+      t.string :num_of_employees
+        t.boolean :still_in_business
+        t.datetime :opening_time
+        t.datetime :closing_time
+        t.timestamps null: false
     end
   end
 end
